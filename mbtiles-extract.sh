aa9797a3ddc9b6f4163ff9c5f411a64dbe815f9e
pettka <75045329+pettka@users.noreply.github.com>
2025-02-21 10:53:53 +0100

    Update mbtiles-extract.sh

--- mbtiles-extract.sh
+++ mbtiles-extract.sh
@@ -1,3 +1,5 @@
+wget https://download.geofabrik.de/europe/czech-republic-latest.osm.pbf -O $(pwd)/czech-republic.osm.pbf
+
 curl https://raw.githubusercontent.com/siwekm/czech-geojson/refs/heads/master/kraje.json | jq '{
   type: "Feature",
   geometry: {
@@ -9,9 +11,17 @@ curl https://raw.githubusercontent.com/siwekm/czech-geojson/refs/heads/master/kr
   }
 }' > prague.json
 
-osmium extract -p prague.json -o /data/openmaptiles/data/cz-prague.osm.pbf /data/openmaptiles/data/czech-republic.osm.pbf
+osmium extract -p prague.json -o $(pwd)/cz-prague.osm.pbf $(pwd)/czech-republic.osm.pbf
 
 
 time docker run --rm --name planetiler -ti -e JAVA_TOOL_OPTIONS="-Xmx24g" -v "/data/openmaptiles/data":/data ghcr.io/onthegomap/planetiler:latest ---osm-path=/data/cz-prague.osm.pbf --nodemap-type=array --storage=mmap --output=/data/cz-prague-$(date '+%y%m%d').mbtiles --download --area=czech-republic
 
-docker run --rm -it -v /data/openmaptiles/data/:/data -p 8888:8080 maptiler/tileserver-gl:latest --file cz-prague-250221.mbtiles
+docker run --rm -it -v $(pwd)/:/data -p 8888:8080 maptiler/tileserver-gl:latest --file cz-prague-$(date '+%y%m%d').mbtiles
+
+
+cat << EOF > Dockerfile
+FROM maptiler/tileserver-gl:latest
+COPY cz-prague-$(date '+%y%m%d').mbtiles /data/tiles.mbtiles
+EOF
+
+docker build -t tileserver-gl:cz-prague-$(date '+%y%m%d') .
